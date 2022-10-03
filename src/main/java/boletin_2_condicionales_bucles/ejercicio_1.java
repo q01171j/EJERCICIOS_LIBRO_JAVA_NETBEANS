afd6003ec56c5e1d72ee04079b2cf73666257255
q01171j <q01171j@upla.edu.pe>
2022-10-02 22:04:36 -0500

    carguis

--- src/main/java/boletin_2_condicionales_bucles/ejercicio_1.java
+++ src/main/java/boletin_2_condicionales_bucles/ejercicio_1.java
@@ -4,10 +4,29 @@
  */
 package boletin_2_condicionales_bucles;
 
+import java.util.Scanner;
+
+// Leer un número y mostrar su cuadrado, repetir el proceso hasta que se introduzca un número negativo.
+
 /**
  *
  * @author Adriano
  */
 public class ejercicio_1 {
-    
+    public static void main(String[] args) {
+        Scanner Entrada = new Scanner(System.in);
+        int num,cuadrado;
+        // num guardará el número que leamos
+        // y cuadrado guardará el cuadrado de num
+        
+        System.out.print("Introduzca número: ");
+        num=Entrada.nextInt();
+        
+        while(num>=0){ // repetimos el proceso mientras el número leído no sea negativo
+            cuadrado=num*num;
+            System.out.println(num+ "² es igual a "+ cuadrado);
+            System.out.print("Introduzca otro número: ");
+            num=Entrada.nextInt(); // volvemos a leer num
+        }
+    }
 }
